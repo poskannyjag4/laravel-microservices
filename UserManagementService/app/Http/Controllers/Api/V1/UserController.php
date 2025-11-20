38713de7376255410ab354d5fec55ad6aace85b3
poskannyjag4 <poskannyjag@gmail.com>
2025-11-20 12:45:25 +0700

    feat(UserController): add UserPostRequest for adding new user and implement this functionality in UserController

--- UserManagementService/app/Http/Controllers/Api/V1/UserController.php
+++ UserManagementService/app/Http/Controllers/Api/V1/UserController.php
@@ -4,27 +4,49 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\UserGetrequest;
+use App\Http\Requests\V1\UserPostrequest;
 use App\Http\Resources\V1\UserResource;
 use App\Repositories\V1\UserRepository;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Prettus\Validator\Exceptions\ValidatorException;
 
 class UserController extends Controller
 {
+
+    function __construct(
+        protected UserRepository $repository,
+    )
+    {
+
+    }
     /**
      * Display a listing of the resource.
      */
-    public function index(UserGetrequest $request, UserRepository $repository): AnonymousResourceCollection
+    public function index(UserGetrequest $request): AnonymousResourceCollection
     {
-        return UserResource::collection($repository->paginate(10));
+        return UserResource::collection($this->repository->paginate(10));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(UserPostrequest $request): JsonResponse|UserResource
     {
-        //
+        $userData =$request->validated();
+        try{
+            return new UserResource($this->repository->create($userData));
+        } catch (ValidatorException $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+            ], 429);
+        }
+        catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+            ], 500);
+        }
     }
 
     /**
--- UserManagementService/routes/api.php
+++ UserManagementService/routes/api.php
@@ -6,5 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('V1')->group(function () {
     Route::controller(UserController::class)->prefix('/users')->group(function () {
         Route::get('/', 'index');
+        Route::post('/', 'store');
     });
+
 });
