0bbdadde0be2c7180c86cc9d3c7bea539a24a712
poskannyjag4 <poskannyjag@gmail.com>
2025-11-20 11:02:48 +0700

    feat: add UserRepository class

---
+++ UserManagementService/app/Repositories/V1/UserRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repositories\V1;
+
+use App\Models\User;
+use Prettus\Repository\Eloquent\BaseRepository;
+
+class UserRepository extends BaseRepository
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function model()
+    {
+        return User::class;
+    }
+
+
+}
