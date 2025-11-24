e8ba33a7057b73dac544f45a4add99acb4907675
poskannyjag4 <poskannyjag@gmail.com>
2025-11-24 11:44:29 +0700

    feat(Category): add fillable and relation 1:n to Task model

--- TaskManagementService/app/Models/Category.php
+++ TaskManagementService/app/Models/Category.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
-    //
+    /**
+     * @var string[]
+     */
+    protected $fillable = ['name'];
+
+    public function tasks(): HasMany{
+        return $this->hasMany(Task::class);
+    }
 }
