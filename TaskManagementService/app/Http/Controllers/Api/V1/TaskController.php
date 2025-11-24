33572ad57b1b5f3efeafe5989eaf879564670f87
poskannyjag4 <poskannyjag@gmail.com>
2025-11-24 12:53:49 +0700

    feat(TaskController): add get task list method to controller

--- TaskManagementService/app/Http/Controllers/Api/V1/TaskController.php
+++ TaskManagementService/app/Http/Controllers/Api/V1/TaskController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\TaskGetRequest;
+use App\Http\Resources\V1\TaskResource;
+use App\Repositories\V1\TaskRepository;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class TaskController extends Controller
 {
-    //
+    public function __construct(
+        protected TaskRepository $repository,
+    ) {}
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): AnonymousResourceCollection
+    {
+        return TaskResource::collection($this->repository->paginate(10));
+    }
+
+
 }
