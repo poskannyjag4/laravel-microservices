3932a0404cd7565eccb28cb1407064e4834d0c75
poskannyjag4 <poskannyjag@gmail.com>
2025-11-24 13:28:08 +0700

    feat(TaskController): add post method for task

--- TaskManagementService/app/Http/Controllers/Api/V1/TaskController.php
+++ TaskManagementService/app/Http/Controllers/Api/V1/TaskController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\TaskGetRequest;
+use App\Http\Requests\V1\TaskPostRequest;
 use App\Http\Resources\V1\TaskResource;
 use App\Repositories\V1\TaskRepository;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Validation\ValidationException;
+use Prettus\Validator\Exceptions\ValidatorException;
 
 class TaskController extends Controller
 {
@@ -23,5 +27,26 @@ class TaskController extends Controller
         return TaskResource::collection($this->repository->paginate(10));
     }
 
+    /**
+     * @param TaskPostRequest $request
+     * @return JsonResponse|TaskResource
+     */
+    public function store(TaskPostRequest $request): JsonResponse|TaskResource
+    {
+        try {
+            $taskData = $request->validated();
+            return new TaskResource($this->repository->create($taskData));
+        } catch (ValidatorException $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+            ], 422);
+        }
+        catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+
+    }
 
 }
