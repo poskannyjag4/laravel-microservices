dd07abf977a1a9f05dd0b1787f18907061c17f99
poskannyjag4 <poskannyjag@gmail.com>
2025-11-24 11:58:43 +0700

    feat: add Task controller and repository

---
+++ TaskManagementService/app/Repositories/V1/TaskRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repositories\V1;
+
+use App\Models\Task;
+use Prettus\Repository\Eloquent\BaseRepository;
+
+/**
+ * @mixin Task
+ */
+class TaskRepository extends BaseRepository
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function model()
+    {
+        return Task::class;
+    }
+}
