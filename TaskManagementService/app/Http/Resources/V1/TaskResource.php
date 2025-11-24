7fd41d5a189e17079d79d4f6c57fe07e5f72b0ae
poskannyjag4 <poskannyjag@gmail.com>
2025-11-24 12:53:05 +0700

    feat: add Task resource

---
+++ TaskManagementService/app/Http/Resources/V1/TaskResource.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'type' => 'tasks',
+            'id' => $this->id,
+
+            'attributes' => [
+                'title' => $this->title,
+                'description' =>$this->description,
+                'status' => $this->status
+            ],
+
+            'relationships' => [
+                'author' => [
+                    'related' => env('USER_SERVICE', 'https:localhost:8000/api/V1/') . '/users/' . $this->user_id,
+                    'data' => [
+                        'type' => 'users',
+                        'id' => $this->user_id
+                    ]
+                ],
+                'category' => [
+                    'related' => env('APP_URL') . '/api/V1/categories' . $this->category_id,
+                    'data' => [
+                        'type' => 'categories',
+                        'id' => $this->category_id
+                    ]
+                ]
+            ]
+        ];
+    }
+}
