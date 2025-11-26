a0dd8c2eb05e7cf646d2ad8523c431ae25156c67
poskannyjag4 <poskannyjag@gmail.com>
2025-11-26 17:10:11 +0700

    feat(UserService): rework UserResource

--- UserManagementService/app/Http/Resources/V1/UserResource.php
+++ UserManagementService/app/Http/Resources/V1/UserResource.php
@@ -19,9 +19,12 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'type' => 'users',
             'id' => $this->id,
-            'name' => $this->name,
-            'email' => $this->email,
+            'attributes' => [
+                'name' => $this->name,
+                'email' => $this->email,
+            ]
         ];
     }
 }
