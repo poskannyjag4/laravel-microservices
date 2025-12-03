2d8ea4dea71ada66a33f78b647edc04113543dd7
poskannyjag4 <poskannyjag@gmail.com>
2025-12-03 12:46:15 +0700

    test(TaskManagementService): add test for show task

--- TaskManagementService/tests/Feature/TaskControllerTest.php
+++ TaskManagementService/tests/Feature/TaskControllerTest.php
@@ -34,4 +34,23 @@ class TaskControllerTest extends TestCase
             ->hasAll(['links', 'meta'])
         );
     }
+
+    public function test_show_returns_task()
+    {
+        $task = Task::factory()->for(Category::factory()->create())->create();
+
+        $response = $this->getJson(self::baseUrl.'/'.$task->id);
+
+        $response->assertOk()->assertJson(fn (AssertableJson $json) => $json->has('data')
+            ->has('data', fn (AssertableJson $json) => $json->where('type', 'tasks')
+                ->has('id')
+                ->has('attributes', fn (AssertableJson $json) => $json->whereType('title', 'string')
+                    ->whereType('description', 'string')
+                    ->whereType('status', 'boolean')
+                )
+                ->has('relationships', fn (AssertableJson $json) => $json->has('author')
+                    ->has('category')
+                )
+            ));
+    }
 }
